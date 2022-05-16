8037584e9eddbe547c2da6d5600254062243830a
Kevin Zhang <kevinzhang425@gmail.com>
2022-05-16 14:23:03 -0700

    KZ - commented out code that I'm not ready for yet

--- frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
+++ frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
+// import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 // import DiningCommonsTable from 'main/components/DiningCommons/DiningCommonsTable';
@@ -9,14 +9,14 @@ export default function RecommendationsIndexPage() {
 
   const _currentUser = useCurrentUser();
 
-  const { data: _diningCommons, error: _error, status: _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/recommendations/all"],
-            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/recommendations/all" },
-      []
-    );
+//   const { data: _diningCommons, error: _error, status: _status } =
+//     useBackend(
+//       // Stryker disable next-line all : don't test internal caching of React Query
+//       ["/api/recommendations/all"],
+//             // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+//             { method: "GET", url: "/api/recommendations/all" },
+//       []
+//     );
 
   return (
     <BasicLayout>
