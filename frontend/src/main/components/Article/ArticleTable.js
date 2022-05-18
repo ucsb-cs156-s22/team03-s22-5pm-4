72ef704f219ede9a99480d07f419682769e54595
Nicholas Wenstad <nwenstad@ucsb.edu>
2022-05-17 23:16:45 -0700

    nw - added Storybook, Table, and Tests for Article

--- frontend/src/main/components/Article/ArticleTable.js
+++ frontend/src/main/components/Article/ArticleTable.js
@@ -1,86 +1,81 @@
-// import OurTable, { ButtonColumn} from "main/components/OurTable";
+import OurTable, { _ButtonColumn} from "main/components/OurTable";
 // import { useBackendMutation } from "main/utils/useBackend";
 // import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
-// // import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 // import { hasRole } from "main/utils/currentUser";
 
 
-// export function cellToAxiosParamsDelete(cell) {
-//     return {
-//         url: "/api/article",
-//         method: "DELETE",
-//         params: {
-//             code: cell.row.values.code
-//         }
-//     }
-// }
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/article",
+        method: "DELETE",
+        params: {
+            code: cell.row.values.code
+        }
+    }
+}
 
-// export default function ArticleTable({ article, _currentUser }) {
+export default function ArticleTable({ article, _currentUser }) {
 
-//     // const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-//     // const editCallback = (cell) => {
-//     //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
-//     // }
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
 
-//     // Stryker disable all : hard to test for query caching
-//     // const deleteMutation = useBackendMutation(
-//     //     cellToAxiosParamsDelete,
-//     //     { onSuccess: onDeleteSuccess },
-//     //     ["/api/article/all"]
-//     // );
-//     // Stryker enable all 
+    // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/article/all"]
+    // );
+    // Stryker enable all 
 
-//     // Stryker disable next-line all : TODO try to make a good test for this
-//     // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
-//     const columns = [
-//         {
-//             Header: 'ID',
-//             accessor: 'id', 
-//         },
-//         {
-//             Header: 'Title',
-//             accessor: 'title',
-//         },
-//         {
-//             Header: 'URL',
-//             accessor: 'url',
-//             // id: 'hasSackMeal', // needed for tests
-//             // accessor: (row, _rowIndex) => String(row.hasSackMeal) // hack needed for boolean values to show up
-//         },
-//         {
-//             Header: 'Explanation',
-//             accessor: 'explanation',
-//             // id: 'url', // needed for tests
-//             // accessor: (row, _rowIndex) => String(row.hasTakeOutMeal) // hack needed for boolean values to show up
+    const columns = [
+        {
+            Header: 'ID',
+            accessor: 'id', 
+        },
+        {
+            Header: 'Title',
+            accessor: 'title',
+        },
+        {
+            Header: 'URL',
+            accessor: 'url',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Date Added',
+            accessor: 'dateAdded',
+        }
+    ];
 
-//         },
-//         {
-//             Header: 'Email',
-//             accessor: 'email',
-//         },
-//         {
-//             Header: 'Date Added',
-//             accessor: 'dateAdded',
-//         }
-//     ];
+    const testid = "ArticleTable";
 
-//     const testid = "ArticleTable";
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     // ButtonColumn("Edit", "primary", editCallback, testid),
+    //     ButtonColumn("Delete", "danger", deleteCallback, testid)
+    // ];
 
-//     // const columnsIfAdmin = [
-//     //     ...columns,
-//     //     // ButtonColumn("Edit", "primary", editCallback, testid),
-//     //     ButtonColumn("Delete", "danger", deleteCallback, testid)
-//     // ];
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
-//     // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = columns;
 
-//     const columnsToDisplay = columns;
-
-//     return <OurTable
-//         data={article}
-//         columns={columnsToDisplay}
-//         testid={testid}
-//     />;
-// };
+    return <OurTable
+        data={article}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
