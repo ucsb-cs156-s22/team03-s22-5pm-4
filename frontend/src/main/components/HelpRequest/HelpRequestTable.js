5ca7896938583dd7bead44bc08f6b4f11e2032e1
arj-singh <arjunsingh@umail.ucsb.edu>
2022-05-18 19:49:34 -0700

    as - fixed duplicate accessor key

--- frontend/src/main/components/HelpRequest/HelpRequestTable.js
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.js
@@ -75,7 +75,7 @@ export default function HelpRequestTable({ helpRequest, currentUser }) {
         },
         {
             Header: 'Solved?',
-            accessor: 'solved',
+            id: 'solved',
             accessor: (row, _rowIndex) => String(row.solved)
         }
     ];
