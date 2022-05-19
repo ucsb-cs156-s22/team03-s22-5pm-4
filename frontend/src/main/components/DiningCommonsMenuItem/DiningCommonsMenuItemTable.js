28f6d1a016d5292cd498dcd2d1288b97b5de3dd4
John Kirchner <j_kirchner@ucsb.edu>
2022-05-18 21:17:56 -0700

    jk - implemented delete in Dining Commons Menu Item

--- frontend/src/main/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable.js
+++ frontend/src/main/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable.js
@@ -1,35 +1,36 @@
-import OurTable, { _ButtonColumn} from "main/components/OurTable";
-import { _useBackendMutation } from "main/utils/useBackend";
-// import { useNavigate } from "react-router-dom";
-import { _hasRole } from "main/utils/currentUser";
+import OurTable, { ButtonColumn} from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/DiningCommonsMenuItemUtils";
+import { _useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
 
 
-// export function cellToAxiosParamsDelete(cell) {
-//     return {
-//         url: "/api/ucsbdiningcommonsmenuitem",
-//         method: "DELETE",
-//         params: {
-//             code: cell.row.values.code
-//         }
-//     }
-// }
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/ucsbdiningcommonsmenuitem",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
 
-export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, _currentUser }) {
+export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, currentUser }) {
 
-    // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/ucsbdiningcommonsmenuitem/all"]
-    // );delet
-    // Stryker enable all 
+    //Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/ucsbdiningcommonsmenuitem/all"]
+    );
+    //Stryker enable all 
 
-    // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    //Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
-            Header: "ID",
+            Header: 'ID',
             accessor: 'id'
         },
         {
@@ -48,15 +49,15 @@ export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, _cur
 
     const testid = "DiningCommonsMenuItemTable";
 
-    // const columnsIfAdmin = [
-    //     ...columns,
-    //     // ButtonColumn("Edit", "primary", editCallback, testid),
-    //     ButtonColumn("Delete", "danger", deleteCallback, testid)
-    // ];
+    const columnsIfAdmin = [
+        ...columns,
+        // ButtonColumn("Edit", "primary", editCallback, testid),
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
 
-    //const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
-    const columnsToDisplay = columns;
+    //const columnsToDisplay = columns;
 
     return <OurTable
         data={diningCommonsMenuItem}
